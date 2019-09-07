f23cda22551e91e82ba20de5f2f61e6b32e6e67a
lgb2002 <cccavin12345@naver.com>
2019-09-07 15:26:52 +0900

    test

--- gmsserver/models.py
+++ gmsserver/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class UserInfo(models.Model):
+	name = models.ForeignKey('auth.User',on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.name
