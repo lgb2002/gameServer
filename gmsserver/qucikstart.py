2469f9a07025953908beb4f737643c8756626abb
lgb2002 <cccavin12345@naver.com>
2019-09-08 13:39:31 +0900

    test

---
+++ gmsserver/qucikstart.py
@@ -0,0 +1,59 @@
+from __future__ import print_function
+import httplib2
+import os
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/drive-python-quickstart.json
+SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Drive API Python Quickstart'
+
+
+def get_credentials():
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'drive-python-quickstart.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else:
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+def main():
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('drive', 'v3', http=http)
+
+    results = service.files().list(
+        pageSize=10,fields="nextPageToken, files(id, name)").execute()
+    items = results.get('files', [])
+    if not items:
+        print('No files found.')
+    else:
+        print('Files:')
+        for item in items:
+            print('{0} ({1})'.format(item['name'], item['id']))
+
+if __name__ == '__main__':
+    main()
--- gmsserver/views.py
+++ gmsserver/views.py
@@ -1,5 +1,72 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from googleapiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file, client, tools
+from google.auth import credentials
+from apiclient import errors
+from apiclient.http import MediaFileUpload
+from googleapiclient.http import MediaIoBaseDownload
+#import mock
+from wsgiref.util import FileWrapper
+from django.views.decorators.csrf import csrf_exempt
+import io, sys, csv
 
+creds = ""
+try:
+	import argparse
+	flags = tools.argparser.parse_args([])
+except ImportError:
+	flags = None
+SCOPES = 'https://www.googleapis.com/auth/drive.file'
+CLIENT_SECRET_FILE = 'gmsserver/client_secret.json'
+CREDENTIAL_FILENAME = 'gmsserver/drive-python-upload.json'
+store = file.Storage(CREDENTIAL_FILENAME)
+creds = store.get()
+if not creds or creds.invalid:
+	print("make new storage data file ")
+	flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+	creds = tools.run_flow(flow, store, flags)
+DRIVE = build('drive', 'v3', http=creds.authorize(Http()))
+#구글 드라이브와 연동
+
+def find_folder(name):
+	page_token = None
+	while True:
+		response = DRIVE.files().list(q="name='"+name+"'",
+		pageToken=page_token).execute()
+		for file in response.get('files', []):
+		# Process change
+			print ('Found file: %s (%s)' % (file.get('name'), file.get('id')))
+			return (file.get('id'))
+		page_token = response.get('nextPageToken', None)
+		if page_token is None:
+			break
+#무슨 용도의 코드인지 이해 불
+#폴더 및 파일 찾기 
+
+
+def file_download(id, name):
+	request = DRIVE.files().get_media(fileId=id)
+	f = open("gmsserver/download/"+name,'wb')
+	wr = csv.writer(f)
+	downloader = MediaIoBaseDownload(f, request)
+	done = False
+	while done is False:
+		status, done = downloader.next_chunk()
+		print("Download "+str(int(status.progress()*100))+"%.")
+		f.close()
+
+@csrf_exempt
 def hello_world(request):
+	if request.method == "POST":
+		date = request.POST.get('date')
+		code = request.POST.get('code')
+		name = date+"-"+code+"c"
+		print(name)
+		file_download(find_folder(name),name)
+		t = open("gmsserver/download/"+name)
+		response =HttpResponse(content_type="text/csv")
+		response['Content-Disposition']='attachment; filename="gmsserver/download/"'+name
+		return response
 	return HttpResponse("Hello World!")
